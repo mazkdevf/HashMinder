51ede40ca8c374c6535a22fd933aca27b92cb789
mazkdevf <79049205+mazk5145@users.noreply.github.com>
2021-10-23 13:35:09 +0300

    Uploaded at 13.35 FI Time

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyAuth_Hash
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			Console.Title = "KeyAuth.com";
+			string argument;
+			keyauthlogo();
+			Console.CursorLeft = 20;
+
+			argument = Console.ReadLine();
+
+			if (argument == "")
+            {
+				Environment.Exit(0);
+            }
+			else
+            {
+				
+            }
+
+			var checksum = GetMD5Checksum(argument);
+
+			Console.Clear();
+			Console.Title = "KeyAuth.com";
+			Console.WriteLine("");
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("             ██   ██ ███████ ██    ██  █████  ██    ██ ████████ ██   ██     ██████  ██████  ███    ███ ");
+			Console.WriteLine("             ██  ██  ██       ██  ██  ██   ██ ██    ██    ██    ██   ██    ██      ██    ██ ████  ████ ");
+			Console.WriteLine("             █████   █████     ████   ███████ ██    ██    ██    ███████    ██      ██    ██ ██ ████ ██ ");
+			Console.WriteLine("             ██  ██  ██         ██    ██   ██ ██    ██    ██    ██   ██    ██      ██    ██ ██  ██  ██ ");
+			Console.WriteLine("             ██   ██ ███████    ██    ██   ██  ██████     ██    ██   ██ ██  ██████  ██████  ██      ██ ");
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.WriteLine("             -----------------------------------------------------------------------------------------");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("             |                           " + checksum + "                            |");
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.WriteLine("             -------------------------------------HASH CHECKER----------------------------------------");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.ReadLine();
+		}
+
+		public static void keyauthlogo()
+        {
+			Console.WriteLine("");
+			Console.WriteLine("             ---------------------------- PLEASE DRAG AND DROP FILE HERE------------------------------");
+			Console.WriteLine("             ██   ██ ███████ ██    ██  █████  ██    ██ ████████ ██   ██     ██████  ██████  ███    ███ ");
+			Console.WriteLine("             ██  ██  ██       ██  ██  ██   ██ ██    ██    ██    ██   ██    ██      ██    ██ ████  ████ ");
+			Console.WriteLine("             █████   █████     ████   ███████ ██    ██    ██    ███████    ██      ██    ██ ██ ████ ██ ");
+			Console.WriteLine("             ██  ██  ██         ██    ██   ██ ██    ██    ██    ██   ██    ██      ██    ██ ██  ██  ██ ");
+			Console.WriteLine("             ██   ██ ███████    ██    ██   ██  ██████     ██    ██   ██ ██  ██████  ██████  ██      ██ ");
+			Console.WriteLine("             --------------------------------------KeyAuth.com----------------------------------------");
+			Console.WriteLine("");
+		}
+
+
+		public static string GetMD5Checksum(string filename)
+		{
+			using (var md5 = System.Security.Cryptography.MD5.Create())
+			{
+				using (var stream = System.IO.File.OpenRead(filename))
+				{
+					var hash = md5.ComputeHash(stream);
+					return BitConverter.ToString(hash).Replace("-", "");
+				}
+			}
+		}
+
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KeyAuth Hash Checker")]
+[assembly: AssemblyDescription("made by mazk#9154")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MD5 Hash Checker")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("3dc63d73-52a3-4372-90d7-4105291f85ec")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
